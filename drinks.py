99acb747adfc55c06125c43a528632f5f2e098be
ggjacky <46613318+ggjacky@users.noreply.github.com>
2019-03-22 18:36:12 -0700

    Create drinks.py

---
+++ drinks.py
@@ -0,0 +1,17 @@
+drinks.py
+Earlier this week
+Mon 10:39 PM
+J
+You uploaded an item
+Text
+drinks.py
+drink_list = [
+	{
+		"name": "Strawberry Milk Tea",
+		"ingredients": {"Strawberry": 200}
+	}, 
+	{
+		"name": "Milk Tea",
+		"ingredients": {"Milk_Tea": 200}
+	}
+]
